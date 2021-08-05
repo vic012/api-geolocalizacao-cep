fd73e4a5e6fae2d0c1eb22b7129b211b60026454
Pedro <henriquevic012@gmail.com>
2021-08-05 12:24:22 -0300

    tratando ceps 00000000

--- api/validate.py
+++ api/validate.py
@@ -13,38 +13,41 @@ class Coordenadas:
 		return self._resultado
 
 	def valido(self):
-		#Consulta o CEP informado pelo usuário
-		api = requests.get(f'https://viacep.com.br/ws/{self._cep}/json/')
+		if (self._cep == "00000000"):
+			return False
+		else:
+			#Consulta o CEP informado pelo usuário
+			api = requests.get(f'https://viacep.com.br/ws/{self._cep}/json/')
 
-		#Verifica se o CEP é válido
-		if (api.status_code == 200):
-			#Transforma os dados em Json
-			endereco = api.json()
-			#rua = endereco['logradouro']
-			#bairro = endereco['bairro']
-			#cidade = endereco['localidade']
+			#Verifica se o CEP é válido
+			if (api.status_code == 200):
+				#Transforma os dados em Json
+				endereco = api.json()
+				#rua = endereco['logradouro']
+				#bairro = endereco['bairro']
+				#cidade = endereco['localidade']
 
-			#Se o endereço não tiver bairro
-			if (endereco['bairro'] == ""):
-				geolocator = Nominatim(user_agent="test_app")
-				location = geolocator.geocode(endereco['localidade'])
-				if (location != None):
-					self._resultado = {"latitude": location.latitude, "longitude": location.longitude}
-					return True
-				else:
-					self._resultado = {"error": "Não consegui identificar as coordenadas da região informada"}
-					return False
-			#Se tiver bairro e cidade
-			else:
-				geolocator = Nominatim(user_agent="test_app")
-				location = geolocator.geocode(endereco['bairro'] + ", " + endereco['localidade'])
-				try:
+				#Se o endereço não tiver bairro
+				if (endereco['bairro'] == ""):
 					geolocator = Nominatim(user_agent="test_app")
 					location = geolocator.geocode(endereco['localidade'])
-					self._resultado = {"latitude": location.latitude, "longitude": location.longitude}
-					return True
-				except:
-					self._resultado = {"error": "Não consegui identificar as coordenadas da região informada"}
-					return False
-		else:
-			return False
+					if (location != None):
+						self._resultado = {"latitude": location.latitude, "longitude": location.longitude}
+						return True
+					else:
+						self._resultado = {"error": "Não consegui identificar as coordenadas da região informada"}
+						return False
+				#Se tiver bairro e cidade
+				else:
+					geolocator = Nominatim(user_agent="test_app")
+					location = geolocator.geocode(endereco['bairro'] + ", " + endereco['localidade'])
+					try:
+						geolocator = Nominatim(user_agent="test_app")
+						location = geolocator.geocode(endereco['localidade'])
+						self._resultado = {"latitude": location.latitude, "longitude": location.longitude}
+						return True
+					except:
+						self._resultado = {"error": "Não consegui identificar as coordenadas da região informada"}
+						return False
+			else:
+				return False
