428230fae834201501619261db008bf395e085a4
Pedro <henriquevic012@gmail.com>
2021-08-05 12:08:02 -0300

    Atualização do projeto

--- api/apps.py
+++ api/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CepGeoConfig(AppConfig):
+class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'cep_geo'
+    name = 'api'
